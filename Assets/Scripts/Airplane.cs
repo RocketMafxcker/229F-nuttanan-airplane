2ee39ceeb9f95d46a5be04a980f662b333d12714
RocketMafxcker <nuttanan.jatu@bumail.net>
2025-03-06 16:36:41 +0700

    EndWeek

---
+++ Assets/Scripts/Airplane.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Airplane : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 40f;
+    [SerializeField] float liftBooster = 0.3f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+    [SerializeField] float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;
+    [SerializeField] float rollPower = 30f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePower);
+        }
+        Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+        float yaw = Input.GetAxis("Horizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("Roll") * rollPower;
+
+        rb.AddTorque(transform.up * yaw);
+        rb.AddTorque(transform.right * pitch);
+        rb.AddTorque(transform.forward * roll);
+    }
+}
